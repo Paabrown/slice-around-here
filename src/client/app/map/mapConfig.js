d8bbe2ff113d7638eed91ae15885c2f8e0a53041
Paul Brown <paabrown@gmail.com>
2017-09-27 15:31:33 -0400

    Google map now plots slice locations on load

--- src/client/app/map/mapConfig.js
+++ src/client/app/map/mapConfig.js
@@ -1,11 +1,25 @@
-window.initMap = function(lat, lng) {
-  var uluru = {lat: -25.363, lng: 131.044};
+import exampleData from '../data/exampleRestaurantData.js';
+
+window.initMap = function() {
+  var uluru = {lat: 40.750379, lng: -73.976837};
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
+    zoom: 15,
     center: uluru
   });
   var marker = new google.maps.Marker({
     position: uluru,
     map: map
   });
+
+  var businesses = exampleData.businesses;
+
+  for (var i = 0; i < businesses.length; i++) {
+    var coords = businesses[i].coordinates
+    var latLng = new google.maps.LatLng(coords.latitude, coords.longitude)
+    var marker = new google.maps.Marker({
+      position: latLng,
+      map: map
+    });
+  }
+
 }
