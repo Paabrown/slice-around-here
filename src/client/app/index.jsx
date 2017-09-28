358128aac8123a190521704ff6174a5b88f7a951
Paul Brown <paabrown@gmail.com>
2017-09-28 19:49:54 -0400

    Eliminate reference to no-longer-used-file

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import App from './components/App.jsx';
 import {render} from 'react-dom';
 import exampleData from './data/exampleRestaurantData.js';
-import G_MAP_API_KEY from './config.js';
 
-render(<App exampleData={exampleData} apiKey={G_MAP_API_KEY}/>, document.getElementById('app'))
+render(<App exampleData={exampleData}/>, document.getElementById('app'))
 
