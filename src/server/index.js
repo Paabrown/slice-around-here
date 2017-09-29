4968ea7a3178632ce21fefc7d99650c7a2450153
Paul Brown <paabrown@gmail.com>
2017-09-29 09:52:37 -0400

    Fix references to /static/ path

--- src/server/index.js
+++ src/server/index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT
 app.use(parser.json());
 
 var path1 = path.join(__dirname, './../../node_modules')
-var path2 = path.join(__dirname, './../../')
+var path2 = path.join(__dirname, './../client/public')
 var path3 = path.join(__dirname, './../client/public/index.html')
 
 console.log('__dirname', __dirname)
@@ -20,11 +20,11 @@ console.log('path2', path2)
 console.log('path3', path3)
 
 app.use('/scripts', express.static(path1));
-app.use('/static', express.static(path2));
+app.use(express.static(path2));
 app.use('/api', router);
-app.get('/', (req, res) => {
-  res.sendFile(path3)
-})
+// app.get('/', (req, res) => {
+//   res.sendFile(path3)
+// })
 
 app.listen(port);
 
