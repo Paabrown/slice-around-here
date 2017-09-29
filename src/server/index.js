ad3d0e8b9db4234b5c62e2ddcba6f28d5840b2cd
Paul Brown <paabrown@gmail.com>
2017-09-29 09:15:55 -0400

    Change root static path to '/static/' path

--- src/server/index.js
+++ src/server/index.js
@@ -9,9 +9,19 @@ const app = express();
 const port = process.env.PORT
 
 app.use(parser.json());
-app.use('/scripts', express.static(path.join(__dirname, './../../node_modules')));
-app.use('/', express.static(path.join(__dirname, './../client/public')));
+
+var path1 = path.join(__dirname, './../../node_modules')
+var path2 = path.join(__dirname, './../client/public')
+
+console.log('path1', path1)
+console.log('path2', path2)
+
+app.use('/scripts', express.static(path1));
+app.use('/static', express.static(path2));
 app.use('/api', router);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './../client/public/index.html'))
+})
 
 app.listen(port);
 
