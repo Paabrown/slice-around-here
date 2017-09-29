55de8fd926617c11d0324e3987ac43b4b8bfcb25
Paul Brown <paabrown@gmail.com>
2017-09-29 09:20:01 -0400

    Add console log to diagnose deploy issue

--- src/server/index.js
+++ src/server/index.js
@@ -12,15 +12,17 @@ app.use(parser.json());
 
 var path1 = path.join(__dirname, './../../node_modules')
 var path2 = path.join(__dirname, './../client/public')
+var path3 = path.join(__dirname, './../client/public/index.html')
 
 console.log('path1', path1)
 console.log('path2', path2)
+console.log('path3', path3)
 
 app.use('/scripts', express.static(path1));
 app.use('/static', express.static(path2));
 app.use('/api', router);
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './../client/public/index.html'))
+  res.sendFile(path3)
 })
 
 app.listen(port);
