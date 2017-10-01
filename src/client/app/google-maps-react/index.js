4b1da9db47e980219e8a7df18180dbad09fcac53
Paul Brown <paabrown@gmail.com>
2017-10-01 11:58:54 -0400

    Commit former dist folder from modified npm module

--- src/client/app/google-maps-react/index.js
+++ src/client/app/google-maps-react/index.js
@@ -1,13 +1,1 @@
-var path = require('path')
-
-console.log('dirname!!!', __dirname);
-
-var maps = path.join(__dirname, './dist/index.js')
-
-console.log('maps', maps);
-
-var Everything = require(maps);
-
-console.log('everything', Everything)
-
-module.exports = Everything;
+module.exports = require('./module/index.js');
