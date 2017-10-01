33b492f7c887b92b6b9af917e053f43cc6bea3de
Paul Brown <paabrown@gmail.com>
2017-10-01 11:06:38 -0400

    Change order of events on buttons

--- src/client/app/components/SliceListEntry.jsx
+++ src/client/app/components/SliceListEntry.jsx
@@ -4,7 +4,7 @@ var SliceListEntry = ({restaurant, onListClick}) => {
     console.log('onListclick 2', onListClick)
 
     return (
-      <button className={'list-entry'} onClick={() => onListClick(restaurant)} onSelect={() => onListClick(restaurant)} onTouchStart={() => onListClick(restaurant)}>
+      <button className={'list-entry'} onTouchStart={() => onListClick(restaurant)} onClick={() => onListClick(restaurant)} >
         <div></div>
         <div>Name: {restaurant.name}</div>
         <div>Rating: {restaurant.rating}</div>
