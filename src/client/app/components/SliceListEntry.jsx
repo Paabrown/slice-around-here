96147d9f84a4cbe68acd583ee0ecbcf6d5dd52bf
Paul Brown <paabrown@gmail.com>
2017-10-01 10:50:26 -0400

    Changed div on botton to buttons

--- src/client/app/components/SliceListEntry.jsx
+++ src/client/app/components/SliceListEntry.jsx
@@ -4,12 +4,12 @@ var SliceListEntry = ({restaurant, onListClick}) => {
     console.log('onListclick 2', onListClick)
 
     return (
-      <div className={'list-entry'} onClick={() => onListClick(restaurant)} onSelect={() => onListClick(restaurant)} onTouchStart={() => onListClick(restaurant)}>
+      <button className={'list-entry'} onClick={() => onListClick(restaurant)} onSelect={() => onListClick(restaurant)} onTouchStart={() => onListClick(restaurant)}>
         <div></div>
         <div>Name: {restaurant.name}</div>
         <div>Rating: {restaurant.rating}</div>
         <div>Address: {restaurant.location.display_address.join(', ')}</div>
-      </div>
+      </button>
     )
 }
 
