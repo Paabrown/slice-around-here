5d91184b112c91b2d41ecde4303dc060a4d4d5d6
Paul Brown <paabrown@gmail.com>
2017-10-01 11:26:53 -0400

    Experiment with changing require statement

--- src/client/app/components/MapContainer.jsx
+++ src/client/app/components/MapContainer.jsx
@@ -1,4 +1,4 @@
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from '../google-maps-react';
+import thing /*{Map, InfoWindow, Marker, GoogleApiWrapper}*/ from '../google-maps-react';
 import React from 'react';
 
 console.log('Map', Map);
